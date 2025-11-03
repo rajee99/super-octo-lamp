2084487ca8828855d2ddae18bdc8ae95d82b4c3c
rajee99 <rajee0585@example.com>
2025-11-03 21:04:59 +0600

    day 3

--- team.py
+++ team.py
@@ -1,2 +1,10 @@
-times = int(input())
+n = int(input())
 
+matrix = [list(map(int, input().split())) for _ in range(n)] #takes input split by spaces, converts to int, converts to list, stores to matrix
+
+count = 0;
+for row in matrix:
+    if sum(row)>=2:
+        count+=1
+
+print(count)
