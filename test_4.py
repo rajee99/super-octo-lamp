5a2caf41363cd56f4291ae4c2f15d4d1490c9b4f
rajee99 <rajee0585@example.com>
2025-11-08 10:51:27 +0600

    test 5

---
+++ test_4.py
@@ -0,0 +1,13 @@
+# Simple Python program: Check if a number is prime
+n = int(input("Enter a number: "))
+is_prime = True
+
+if n <= 1:
+    is_prime = False
+else:
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            is_prime = False
+            break
+
+print(n, "is prime." if is_prime else "is not prime.")
