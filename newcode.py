abb9b4cfe8aa104fd442c9cfefb4430015aa41e2
Al Rajee <rajee0585@example.com>
2025-11-08 11:04:37 +0600

    new_commit

---
+++ newcode.py
@@ -0,0 +1,8 @@
+# Simple Python program: Fibonacci sequence
+n = int(input("Enter number of terms: "))
+a, b = 0, 1
+
+print("Fibonacci sequence:")
+for _ in range(n):
+    print(a, end=" ")
+    a, b = b, a + b
