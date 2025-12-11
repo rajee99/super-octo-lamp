f9fb4033ae30365233fdcd18c7e366a7417fddd3
Al Rajee <rajee0585@gmail.com>
2025-12-11 23:31:41 +0600

    day 41

--- I_Wanna_Be_the_Guy.py
+++ I_Wanna_Be_the_Guy.py
@@ -3,7 +3,7 @@ n = int(input())
 p = list(map(int, input().split()))
 q = list(map(int, input().split()))
 
-levels = set(p[1:] + q[1:])
+levels = set(p[1:] + q[1:]) #set e union korlam 1st index bad e
 
 if len(levels) == n:
     print("I become the guy.")
