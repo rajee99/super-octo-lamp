6cdd625bc78f4e801b82f4751af628051d827b5e
Al Rajee <rajee0585@gmail.com>
2025-12-12 23:04:05 +0600

    day 42

--- Arrival_of_the_General.py
+++ Arrival_of_the_General.py
@@ -1,6 +1,18 @@
-b = int(input())
+n = int(input())
 a = list(map(int, input().split()))
 
+max_h = max(a)
+min_h = min(a)
 
-max_height = max(a)
-min_height = min(a)
+# leftmost max
+max_index = a.index(max_h)
+
+# rightmost min
+min_index = n - 1 - a[::-1].index(min_h)
+
+ans = max_index + (n - 1 - min_index)
+
+if max_index > min_index:
+    ans -= 1
+
+print(ans)
